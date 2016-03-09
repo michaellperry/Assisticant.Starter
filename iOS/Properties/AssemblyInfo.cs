bf8a02b4d64631bac4868ba3f0ce2f14d6435ccf
Michael L Perry <michael@qedcode.com>
2016-03-08 22:39:50 -0600

    Updated project files.

--- iOS/Properties/AssemblyInfo.cs
+++ iOS/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Assisticant.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bd4c5c81-0476-4d2a-b3ff-ff889ab78b1f")]
+[assembly: Guid("89b554eb-2886-475f-9672-f90fae10c4f2")]
 
 // Version information for an assembly consists of the following four values:
 //
